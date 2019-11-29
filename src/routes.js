d4ee69c71e33179562cf2bca5c6d2d1df0697d80
lucadebeir <l.debeir@me.com>
2019-11-29 15:51:02 +0100

    update calculService

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const router = express.Router()
-const calcul = require('./calcul')
+const calcul = require('./service/calculService')
 
 const { check, validationResult } = require('express-validator/check')
 
