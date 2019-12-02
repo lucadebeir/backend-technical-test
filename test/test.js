e096f4c92c2c3735ca556d899957b7f3c97577e8
lucadebeir <l.debeir@me.com>
2019-12-02 13:37:52 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,46 +1,123 @@
 var expect    = require("chai").expect;
 var calculService = require('../src/service/calculService');
 
-const testData = {
+const testgoodData = {
     firstname : 'Luca',
     lastname : 'Debeir',
     annualSalary : 60050,
     superRate : 9
 }
 
+const testwrongData = {
+  firstname : 'Debeir',
+  lastname : 'Luca',
+  annualSalary : 60050,
+  superRate : 9
+}
 
-describe('Test function of calculService ', function () {
+//GOOD DATA
+describe('Test function of calculService with good data', function () {
     it('Test name', function() {
-      var name = calculService.name(testData.lastname, testData.firstname);
+      var name = calculService.name(testgoodData.lastname, testgoodData.firstname);
       
       expect(name).to.equal("Debeir Luca");
     })
 
     it('Test gross-income', function () {
-      var grossIncome = calculService.rounded(calculService.grossIncome(testData.annualSalary,12));
+      var grossIncome = calculService.rounded(calculService.grossIncome(testgoodData.annualSalary,12));
 
       expect(grossIncome).to.equal(5004);
     })
 
     it('Test income-tax', function () {
-      var incomeTax = calculService.rounded(calculService.incomeTax(testData.annualSalary));
+      var incomeTax = calculService.rounded(calculService.incomeTax(testgoodData.annualSalary));
 
       expect(incomeTax).to.equal(922);
     })
 
     it('Test net-income', function () {
-      var grossIncome = calculService.rounded(calculService.grossIncome(testData.annualSalary,12));
-      var incomeTax = calculService.rounded(calculService.incomeTax(testData.annualSalary));
+      var grossIncome = calculService.rounded(calculService.grossIncome(testgoodData.annualSalary,12));
+      var incomeTax = calculService.rounded(calculService.incomeTax(testgoodData.annualSalary));
       var netIncome = calculService.rounded(calculService.netIncome(grossIncome,incomeTax));
 
       expect(netIncome).to.equal(4082);
     })
 
     it('Test super-amount', function () {
-      var grossIncome = calculService.rounded(calculService.grossIncome(testData.annualSalary,12));
-      var superAmount = calculService.rounded(calculService.superAmount(grossIncome,testData.superRate));
+      var grossIncome = calculService.rounded(calculService.grossIncome(testgoodData.annualSalary,12));
+      var superAmount = calculService.rounded(calculService.superAmount(grossIncome,testgoodData.superRate));
 
       expect(superAmount).to.equal(450);
     });
 });
 
+//WRONG RESULT
+describe('Test function of calculService with wrong data', function () {
+  it('Test name', function() {
+    var name = calculService.name(testwrongData.lastname, testwrongData.firstname);
+    
+    expect(name).not.to.equal("Debeir Luca");
+  })
+
+  it('Test gross-income', function () {
+    var grossIncome = calculService.rounded(calculService.grossIncome(testwrongData.annualSalary,12));
+
+    expect(grossIncome).not.to.equal(5003);
+  })
+
+  it('Test income-tax', function () {
+    var incomeTax = calculService.rounded(calculService.incomeTax(testwrongData.annualSalary));
+
+    expect(incomeTax).not.to.equal(921);
+  })
+
+  it('Test net-income', function () {
+    var grossIncome = calculService.rounded(calculService.grossIncome(testwrongData.annualSalary,12));
+    var incomeTax = calculService.rounded(calculService.incomeTax(testwrongData.annualSalary));
+    var netIncome = calculService.rounded(calculService.netIncome(grossIncome,incomeTax));
+
+    expect(netIncome).not.to.equal(4081);
+  })
+
+  it('Test super-amount', function () {
+    var grossIncome = calculService.rounded(calculService.grossIncome(testwrongData.annualSalary,12));
+    var superAmount = calculService.rounded(calculService.superAmount(grossIncome,testwrongData.superRate));
+
+    expect(superAmount).not.to.equal(449);
+  });
+});
+
+
+//TEST FUNCTIONS MISSING ONE ARGUMENT
+describe('Test function of calculService with missing one argument', function () {
+  it('Test name', function() {
+    var name = calculService.name(testgoodData.firstname);
+    
+    expect(name).not.to.equal("Debeir Luca");
+  })
+
+  it('Test gross-income', function () {
+    var grossIncome = calculService.rounded(calculService.grossIncome(testgoodData.annualSalary));
+
+    expect(grossIncome).not.to.equal(5004);
+  })
+
+  it('Test income-tax', function () {
+    var incomeTax = calculService.rounded(calculService.incomeTax());
+
+    expect(incomeTax).not.to.equal(922);
+  })
+
+  it('Test net-income', function () {
+    var grossIncome = calculService.rounded(calculService.grossIncome(testgoodData.annualSalary));
+    var netIncome = calculService.rounded(calculService.netIncome(grossIncome));
+
+    expect(netIncome).not.to.equal(4082);
+  })
+
+  it('Test super-amount', function () {
+    var superAmount = calculService.rounded(calculService.superAmount(testgoodData.superRate));
+
+    expect(superAmount).not.to.equal(450);
+  });
+});
